85c018746b65067ae89daab24ed86d421c48591f
DaMan <jcazdev100@protonmail.com>
2022-07-17 23:58:03 -0700

    Fixed cors origin

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require('dotenv').config()
 
 app.use(cors())
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "https://jesuscazares-portfolio-manager.netlify.app/");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
     next();
