1ddbb4188e04100c41f819bade8450b30c305f0e
DaMan <jcazdev100@protonmail.com>
2022-07-17 23:01:05 -0700

    Fix CORS

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require("express") 
+const express = require("express")
 const cors = require('cors')
 const stockRoutes = require('./routes/stockRoutes')
 
@@ -7,6 +7,12 @@ const app = express()
 require('dotenv').config()
 
 app.use(cors())
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+    next();
+});
 app.use(express.json())
 
 //API Routes
@@ -15,6 +21,6 @@ app.use('/api/stocks', stockRoutes)
 //Port listening
 const PORT = process.env.PORT || 5000;
 
-const server = app.listen(PORT, ()=> {
+const server = app.listen(PORT, () => {
     console.log(`Server connected`)
-})
+})  
